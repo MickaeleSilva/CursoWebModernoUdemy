953e70ccfd208a4719920de1cd3d0539d8ba5352
Mickaele Silva <Mickaeleabildi@gmail.com>
2024-04-10 17:00:49 -0300

    Alterando Nome de arquivos

--- 3. Funções/FuncoesCallback/callbackExemploBrowser.js
+++ 3. Funções/FuncoesCallback/callbackExemploBrowser.js
@@ -1,4 +1,4 @@
 // Exemplo de callback no browser: Função que acessa os elementos a partir de uma tag
-document.getElementsByTagName('body')[0].oneclick = function (e) {
+document.getElementsByTagName('body')[0].onclick = function (e) {
     console.log('o evento ocorreu!')
-}
+} 
